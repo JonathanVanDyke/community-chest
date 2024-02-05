a73ea4df0e5b1cd33c1707ee3e7f6bc54b2fce09
Jonathan Van Dyke <jfv213@gmail.com>
2024-02-05 00:03:57 -0500

    Refactor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,64 +13,9 @@ export default async function Home() {
   const session = await getServerAuthSession();
 
   return (
-    <main className="h-100 flex flex-row items-center justify-between bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="fixed	 top-0 z-30 flex h-full w-72 border-r-2 border-solid border-red-500 bg-red-800">
-        <ScrollArea className="h-full w-full py-6 pr-6 lg:py-8">
-          <div className="align-center flex items-center  justify-center">
-            <Image
-              priority
-              width={80}
-              color={"yellow"}
-              src={chestIcon}
-              alt="chest"
-            />
-            <div className="align-center m-3 flex flex-col items-center justify-center font-black text-red-200">
-              <div>COMMUNITY</div>
-              <div>CHEST</div>
-            </div>
-          </div>
-          <br />
-          <DocsSidebarNav
-            items={[
-              {
-                title: "Getting Started",
-                items: [
-                  {
-                    title: "T3-example-page",
-                    href: "/t3-example-page",
-                    items: [],
-                  },
-                ],
-              },
-              {
-                title: "Components",
-                items: [
-                  {
-                    title: "Command",
-                    href: "/docs/components/command",
-                    items: [],
-                  },
-                  {
-                    title: "Tooltip",
-                    href: "/docs/components/tooltip",
-                    items: [],
-                  },
-                ],
-              },
-            ]}
-          />
-        </ScrollArea>
-      </div>
-      <div className="w-72" />
-
-      <div className="flex min-h-screen w-full flex-col items-center justify-center overflow-scroll bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-        <div className="h-96">Hi</div>
-        <div className="h-96">Hi</div>
-
-        <div className="h-96">Hi</div>
-        <div className="h-96">Hi</div>
-      </div>
-    </main>
+    <div>
+      <div className="h-96">Welcome!</div>
+    </div>
   );
 }
 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,21 @@ export default {
       fontFamily: {
         sans: ["var(--font-sans)", ...fontFamily.sans],
       },
+      colors: {
+        'aqua': {
+            '50': '#effefa',
+            '100': '#c7fff2',
+            '200': '#8affe4',
+            '300': '#50f8d7',
+            '400': '#1de4c4',
+            '500': '#04c8ab',
+            '600': '#00a18d',
+            '700': '#058071',
+            '800': '#0a655c',
+            '900': '#0d544c',
+            '950': '#003330',
+        },
+      }
     },
   },
   plugins: [],
