d89d1bdb1178a64c0f169f72b0a393eae2567398
Jonathan Van Dyke <jfv213@gmail.com>
2024-02-05 01:16:37 -0500

    Refactor

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,13 +3,7 @@ import "~/styles/globals.css";
 import { Inter } from "next/font/google";
 
 import { TRPCReactProvider } from "~/trpc/react";
-import { ScrollArea } from "./ui/ScrollArea";
-import { SidebarNav } from "./ui/SidebarNav";
-
-import chestIcon from "../../public/chestIcon.svg";
-
-import Image from "next/image";
-import { Separator } from "./ui/Separator";
+import Sidebar from "./_components/layout/Sidebar";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -32,57 +26,8 @@ export default function RootLayout({
       <body className={`font-sans ${inter.variable}`}>
         <TRPCReactProvider>
           <main className="h-100 from-aqua-950 to-aqua-10 flex flex-row items-center justify-between bg-gradient-to-b">
-            <div className="bg-aqua-100 border-aqua-90 text-aqua-950 fixed top-0 flex h-full w-72 border-r-2 border-solid">
-              <ScrollArea className="h-full w-full py-6 pr-6 lg:py-8">
-                <a
-                  href={"/"}
-                  className="align-center flex items-center justify-center"
-                >
-                  <Image
-                    priority
-                    width={80}
-                    color={"aqua"}
-                    src={chestIcon}
-                    alt="chest"
-                  />
-                  <div className="align-center text-aqua-950 m-3 flex flex-col items-center justify-center font-black">
-                    <div>COMMUNITY</div>
-                    <div>CHEST</div>
-                  </div>
-                </a>
-                <br />
-                <Separator className="m-4 bg-black " />
-                <SidebarNav
-                  items={[
-                    {
-                      title: "Getting Started",
-                      items: [
-                        {
-                          title: "T3-example-page",
-                          href: "/t3-example-page",
-                          items: [],
-                        },
-                      ],
-                    },
-                    {
-                      title: "Components",
-                      items: [
-                        {
-                          title: "Command",
-                          href: "/docs/components/command",
-                          items: [],
-                        },
-                        {
-                          title: "Tooltip",
-                          href: "/docs/components/tooltip",
-                          items: [],
-                        },
-                      ],
-                    },
-                  ]}
-                />
-              </ScrollArea>
-            </div>
+            <Sidebar />
+
             <div className="w-96" />
             <div className="from-aqua-950 to-aqua-10 flex min-h-screen w-full flex-col items-center justify-center overflow-scroll bg-gradient-to-b text-white">
               {children}
