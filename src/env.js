7b9c360b2aceaafa2bb19969ba3ba55dc7e3379f
Jonathan Van Dyke <jfv213@gmail.com>
2024-02-12 23:10:16 -0500

    Add jwt env var

--- src/env.js
+++ src/env.js
@@ -7,6 +7,7 @@ export const env = createEnv({
    * isn't built with invalid env vars.
    */
   server: {
+    JWT_SECRET: z.string(),
     DATABASE_URL: z
       .string()
       .url()
@@ -46,6 +47,7 @@ export const env = createEnv({
    * middlewares) or client-side so we need to destruct manually.
    */
   runtimeEnv: {
+    JWT_SECRET: process.env.JWT_SECRET,
     DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
