493f61de92ab388676f460bd0c02a37a7c2b650b
Devin Visslailli <devin.viss@gmail.com>
2024-03-01 13:05:49 -0500

    Move Sidebar to ui; add sticky position; add close on click for nav items

--- src/app/ui/Sidebar.tsx
+++ src/app/ui/Sidebar.tsx
@@ -1,10 +1,11 @@
-import { ScrollArea } from "~/app/ui/ScrollArea";
-import { SidebarNav } from "../../ui/SidebarNav";
-
+"use client";
+import Image from "next/image";
 import { Sheet, SheetContent, SheetTrigger } from "~/components/ui/sheet";
 
-import Image from "next/image";
-import svgColorFilter from "~/styles/svgColorFilters";
+import { ScrollArea } from "~/app/ui/ScrollArea";
+import { SidebarNav } from "./SidebarNav";
+import { useState } from "react";
+import svgColorFilters from "~/styles/svgColorFilters";
 
 const sideBarItems = [
   {
@@ -29,24 +30,29 @@ const sideBarItems = [
 ];
 
 const Sidebar = () => {
+  const [open, setOpen] = useState(false);
   return (
     <>
-      <div className="fixed left-2 top-2 z-50 h-24 sm:hidden">
-        <Sheet>
-          <SheetTrigger>
-            <Image
-              src="/icons/menu.svg"
-              alt="menu"
-              width="40"
-              height="40"
-              style={{
-                filter: svgColorFilter.white,
-              }}
-              className="mt-3 h-8 w-8 xs:h-10 xs:w-10"
-            />
-          </SheetTrigger>
+      <div className="sticky left-0 top-0 z-10 h-fit w-full sm:hidden">
+        <Sheet open={open} onOpenChange={setOpen}>
+          <div className="xs:h-16 flex h-24 w-full items-center border-b-2 border-slate-900 bg-gradient-to-b from-aqua-800 to-aqua-950 px-5">
+            <SheetTrigger className="mr-4 h-8 w-8 grow-0">
+              <Image
+                src="/icons/menu.svg"
+                alt="menu"
+                width="40"
+                height="40"
+                style={{
+                  filter: svgColorFilters.white,
+                }}
+              />
+            </SheetTrigger>
+            <h1 className="-ml-8 w-full px-8 text-center text-3xl text-white">
+              Community Chest
+            </h1>
+          </div>
           <SheetContent side="left" className="w-full border-none p-0">
-            <SidebarContent />
+            <SidebarContent onNavItemClick={() => setOpen(false)} />
           </SheetContent>
         </Sheet>
       </div>
@@ -57,12 +63,16 @@ const Sidebar = () => {
   );
 };
 
-const SidebarContent = () => (
+const SidebarContent = ({
+  onNavItemClick,
+}: {
+  onNavItemClick?: () => void;
+}) => (
   <div className="border-aqua-90 flex h-full w-full bg-aqua-100 text-aqua-950">
     <ScrollArea className="h-full w-full py-6 lg:py-8">
       <Logo />
       <br />
-      <SidebarNav items={sideBarItems} />
+      <SidebarNav items={sideBarItems} onNavItemClick={onNavItemClick} />
     </ScrollArea>
   </div>
 );
@@ -75,7 +85,7 @@ const Logo = () => (
       width="80"
       height="80"
       style={{
-        filter: svgColorFilter.aqua[950],
+        filter: svgColorFilters.aqua[950],
       }}
     />
     <div className="align-center m-3 flex flex-col items-center justify-center font-black text-aqua-950">
