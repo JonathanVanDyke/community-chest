90e3e177e381d815883fdf9407282cf02895c776
Jonathan Van Dyke <jfv213@gmail.com>
2024-03-10 15:56:27 -0400

    Remove signup form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,9 @@
 "use client";
 
-import { SignUp } from "~/app/_components/create-post";
-
 export default function Home() {
   return (
     <div>
       <div className="h-96">hi</div>
-      <SignUp />
     </div>
   );
 }
